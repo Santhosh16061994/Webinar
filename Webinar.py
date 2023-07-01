7f7fa07ac8bca85824c965cec981bbef5dd6c4a9
SANTHOSH T N <120988989+Santhosh16061994@users.noreply.github.com>
2023-07-02 02:50:01 +0530

    Update Webinar.py

--- Webinar.py
+++ Webinar.py
@@ -4,7 +4,6 @@ Created on Sun Jul  2 02:33:58 2023
 
 @author: Santhosh T N
 """
-
 import streamlit as st
 
 def main():
@@ -25,10 +24,20 @@ def main():
         else:
             st.warning("Please fill in all fields.")
 
+    # View Registrations button
+    if st.button("View Registrations"):
+        view_registrations()
+
 def save_registration(name, email, webinar_topic):
-    # Save registration details to a database or file
-    # Implement your saving logic here
-    pass
+    with open("registrations.txt", "a") as file:
+        file.write(f"Name: {name}, Email: {email}, Webinar Topic: {webinar_topic}\n")
+
+def view_registrations():
+    with open("registrations.txt", "r") as file:
+        registrations = file.readlines()
+    st.write("Registered Users:")
+    for reg in registrations:
+        st.write(reg.strip())
 
 if __name__ == '__main__':
     main()
